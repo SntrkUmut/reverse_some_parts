55d8e293b8c4cddcc9e4452f6d36cd37b1af6bc6
SntrkUmut <umutsntrk.0@gmail.com>
2024-01-04 01:26:36 +0300

    first commit

---
+++ reverse_some_parts.py
@@ -0,0 +1,39 @@
+'''
+You have a text that some of the words in reverse order.
+The text also contains some words in the correct order, and they are wrapped in parenthesis.
+Write a function fixes all of the words and,
+remove the parenthesis that is used for marking the correct words.
+
+Your function should return the same text defined in the constant CORRECT_ANSWER
+'''
+
+
+INPUT = ("nhoJ (Griffith) nodnoL saw (an) (American) ,tsilevon "
+         ",tsilanruoj (and) laicos .tsivitca ((A) reenoip (of) laicremmoc "
+         "noitcif (and) naciremA ,senizagam (he) saw eno (of) (the) tsrif "
+         "(American) srohtua (to) emoceb (an) lanoitanretni ytirbelec "
+         "(and) nrae a egral enutrof (from) ).gnitirw")
+
+CORRECT_ANSWER = "John Griffith London was an American novelist, journalist, and social activist. (A pioneer of commercial fiction and American magazines, he was one of the first American authors to become an international celebrity and earn a large fortune from writing.)"
+
+
+def fix_text(mystr):
+    #Write your alghoritm here.
+    input_strings = mystr.split("\n")
+    correctAnswer=[]
+    for deneme in input_strings:
+        x=deneme.split()
+        for i in range(len(x)):
+            if x[i].startswith("(("):
+                x[i]= x[i][1:-1]
+            elif not x[i].startswith("("):
+                x[i]=x[i][::-1]
+            else:
+                x[i] = x[i].replace("(", "").replace(")", "")
+        correctAnswer.append(' '.join(x))
+    result = " ".join(correctAnswer)
+    return result
+
+
+if __name__ == "__main__":
+    print("Correct!" if fix_text(INPUT) == CORRECT_ANSWER else "Sorry, it does not match with the correct answer.")
